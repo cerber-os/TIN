b48df57e6051ce35d6addbd042dfda924698a030
Paweł Wieczorek <contact@cerberos.pl>
2021-01-10 17:03:20 +0100

    Fix double-free in unlink command (#18)

--- server/client_handle.c
+++ server/client_handle.c
@@ -275,14 +275,14 @@ static int _process_client_message(int socket_fd, struct mynfs_datagram_t* packe
             free(path_name);
 
 
-            int ret = unlink(path_name);
+            int ret = unlink(sanitized_path);
             if(ret < 0) {
-                nfs_log_error(logger, "Failed to unlink file `%s` - errno: %d", path_name, errno);
-                free(path_name);
+                nfs_log_error(logger, "Failed to unlink file `%s` - errno: %d", sanitized_path, errno);
+                free(sanitized_path);
                 return (errno > 0) ? -1 * errno : errno;
             }
 
-            free(path_name);
+            free(sanitized_path);
             return MYNFS_SUCCESS;
         }
         break;
