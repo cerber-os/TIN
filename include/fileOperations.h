dad337f0a3b71abc3e0bc70672f926b564b6fe91
Paweł Wieczorek <contact@cerberos.pl>
2021-01-14 01:06:03 +0100

    Add include file for shared library

---
+++ include/fileOperations.h
@@ -0,0 +1,26 @@
+#ifndef FILEOPERATIONS_H
+#define FILEOPERATIONS_H
+
+#include <sys/stat.h>
+
+
+// Special flag to function mynfs_open, used to lock access to file
+#define O_MYNFS_LOCK        0x8000
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    int mynfs_open(char *host, char *path, int oflag, int mode, int *socketFd);
+    int mynfs_read(int socketFd, int fd, void *buf, size_t count);
+    ssize_t mynfs_write(int socketFd, int fd, void *buf, size_t count);
+    off_t mynfs_lseek(int socketFd, int fd, off_t offset, int whence);
+    int mynfs_close(int socketFd, int fd);
+    int mynfs_unlink(char *host, char *pathname);
+    int mynfs_fstat(int socketFd, int fd, struct stat *buf);
+
+#ifdef __cplusplus   
+}
+#endif
+
+#endif //FILEOPERATIONS_H
